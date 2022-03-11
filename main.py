a09fc477d39ef9d6d7044f485ee3050082fdefc6
Jonathon Quick <jonathon@spliff.li>
2022-03-11 09:58:58 +0100

    moved all supertrend strategy logic out of main() into strategies.supertrend

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import asyncio
-import ccxt
 import pandas as pd
 import warnings
 from configparser import ConfigParser
 
-from core.indicators import supertrend
-from core.utils import check_signals, create_synthetic_pair
 from core.position_manager import Position, PositionManager
 from strategies.strategy_base import StrategyBase
 
 from scripts.kucoin_extended import KuCoinExtended
-
+"""
 pd.set_option('display.max_rows', None)
 warnings.filterwarnings('ignore')
-
+"""
 config = ConfigParser()
 config.read("user-config.ini")
 
@@ -37,14 +34,8 @@ async def main(base_pair=config['Global Settings']['base_pair_default'],
     while running:
 
         # This code is for one timeframe. Later, do the same iterated for each timeframe.
-        base_bars = exchange.fetch_ohlcv(base_pair, timeframe="1m", limit=50)
-        quote_bars = exchange.fetch_ohlcv(quote_pair, timeframe="1m", limit=50)
 
-        pair = create_synthetic_pair(base_bars, quote_bars)
-
-        supertrend_data = supertrend(pair)
-
-        signal = check_signals(supertrend_data)
+        signal = strategy.get_current_signal()
 
         # [DEBUG] Un-comment one of the three lines below to force a signal:
         # signal = 'LONG'
@@ -55,7 +46,7 @@ async def main(base_pair=config['Global Settings']['base_pair_default'],
 
         if signal is not None and signal != 'CLOSE':
 
-            if manager.get_current_position() is None:
+            if strategy.manager.get_current_position() is None:
                 position = Position(base_pair, quote_pair,
                                     direction=signal,
                                     order_type='limit').open()
--- strategies/strategy_base.py
+++ strategies/strategy_base.py
@@ -1,9 +1,16 @@
 import sys
 from configparser import ConfigParser
 import os
+
 print(os.getcwd())
 sys.path.append(os.getcwd())
+
+from main import config
+from core.position_manager import PositionManager, Position
 from core.utils import get_synth_pair_symbol, create_synthetic_pair
+from pandas import DataFrame
+
+
 """
 config = configparser.ConfigParser
 user_config_path = os.path.join(os.getcwd(), 'user-config.ini')
@@ -13,29 +20,30 @@ user_config.read(config_path)
 
 class StrategyBase:
 
-    def __init__(self, config: ConfigParser, base_pair: str, quote_pair: str, timeframes=[]):
+    def __init__(self, base_pair: str, quote_pair: str, timeframes=[], paper_trade=False, config: ConfigParser = config):
         self.config = config
         self.exchange_id = config['Global Settings']['exchange']
-
         self.name = None
         self.current_signal = None
         self.base_pair = base_pair
         self.quote_pair = quote_pair
         self.synth_pair = get_synth_pair_symbol(base_pair, quote_pair)
-        self.paper_trade = False
+        self.paper_trade = paper_trade
         self.timeframes = []
+        self.manager = PositionManager
 
         for timeframe in timeframes:
             if timeframe.lower in ['1m', '5m', '15m', '1h', '4h', '1d', '1w']:
                 self.timeframes.append(timeframe)
 
-    def get_current_signal(self):
-        return self.current_signal
-
     def get_synth_ohlcv(self, timeframe="1m", limit=50):
         base_bars = self.fetch_ohlcv(self.base_pair, timeframe=timeframe, limit=limit)
         quote_bars = self.fetch_ohlcv(self.quote_pair, timeframe=timeframe, limit=limit)
 
-        ohlcv = create_synthetic_pair(base_bars, quote_bars)
+        ohlcv: DataFrame = create_synthetic_pair(base_bars, quote_bars)
 
         return ohlcv
+
+    def get_current_signal(self):
+
+        return self.current_signal
