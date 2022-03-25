2ff2ecd5ed40f066592cecca399607659094bd98
Jonathon Quick <jonathon@spliff.li>
2022-03-25 20:20:01 +0100

    refactored borrowing into its own method in position_manager.Position to simplify open_long & open_short

--- core/position_manager.py
+++ core/position_manager.py
@@ -28,9 +28,9 @@ class Position(Config):
 
     borrow_info = {
         "currency": None,
-        "borrow_timestamp": None,
+        "borrow_timestamp_utc": None,
         "borrow_qty": None,
-        "repay_timestamp": None,
+        "repay_timestamp_utc": None,
         "repay_qty": None
     }
 
@@ -38,12 +38,12 @@ class Position(Config):
 
     trades_info = {
         "open": {
-            "base_pair": {"timestamp": None, "fillPrice": None},
-            "quote_pair": {"timestamp": None, "fillPrice": None},
+            "base_pair": {"side": '', "timestamp": '', "fillPrice": ''},
+            "quote_pair": {"side": '', "timestamp": '', "fillPrice": ''},
         },
         "close": {
-            "base_pair": {"timestamp": None, "fillPrice": None},
-            "quote_pair": {"timestamp": None, "fillPrice": None},
+            "base_pair": {"side": '', "timestamp": '', "fillPrice": ''},
+            "quote_pair": {"side": '', "timestamp": '', "fillPrice": ''},
         },
     }
 
@@ -66,9 +66,13 @@ class Position(Config):
 
         super().__init__(params, config_filepath)
         self.status['msg'] = 'INIT Position'
+        self.status['trades_info'] = self.trades_info
 
         self.order_type = order_type
+        self.status['order_type'] = self.order_type
+
         self.direction = direction
+        self.status['direction'] = self.direction
 
         if self.direction == 'LONG':
             self.status['msg'] = (f"Opening LONG Position on {self.synth_pair}\n"
@@ -98,69 +102,21 @@ class Position(Config):
         #           KuCoin so there needs to be at least two approaches for margin
         #           trading ie. with or without knowing that.
 
+        self.trades_info['open']['base_pair']['side'] = 'buy'
+        self.trades_info['open']['base_pair']['symbol'] = self.base_pair
+
+        self.trades_info['open']['quote_pair']['side'] = 'sell'
+        self.trades_info['open']['quote_pair']['symbol'] = self.quote_pair
+
         self.borrow_coin = self.get_borrow_coin(self.synth_pair_tuple, 'LONG')
         self.borrow_info['currency'] = self.borrow_coin
+
         self.status['msg'] = f"Borrow coin for {self.synth_pair} is: {self.borrow_coin}"
+        self.status['borrow_coin'] = self.borrow_coin
+        self.status['borrow_info'] = self.borrow_info
+        self.status['ok'] = True
 
-        self.status['msg'] = f"Checking which methods {self.exchange_name} has for margin...\n"
-        if self.exchange.has['fetchBorrowRate']:
-            self.status['msg'] = f"{self.exchange_name} has fetchBorrowRate"
-            # available_margin = self.exchange.fetch_borrow_rate(self.borrow_coin)
-        else:
-            self.status['msg'] = (f"{self.exchange_name} doesn't have fetchBorrowRate.\n"
-                           f"Checking if {self.exchange_name} has fetchMaxBorrowSize..\n"
-                           f"Note: This is a non-standard method. If possible, find a way "
-                           f"to use fetchBorrowRate instead, or some other method. "
-                           f"The only exchange which has the method `fetchMaxBorrowSize`"
-                           " is kucoin_extended, but if others are added, this will find it.")
-
-            if self.exchange.has['fetchMaxBorrowSize']:
-                self.status['msg'] = f"{self.exchange_name} has fetchMaxBorrowSize."
-
-                max_borrow_size = float(self.exchange.fetch_max_borrow_size(self.borrow_coin))
-                self.status['msg'] = f"\nMax borrow amount: {max_borrow_size}  {self.borrow_coin}"
-                print("--------------------------------\n"
-                      f"{self.status['msg']}")
-            else:
-                raise Exception(f"{self.exchange_name} doesn't have fetchMaxBorrowSize or fetchBorrowRate. Can't proceed.")
-
-        # ----------------------------------------
-        # Step 2: If prompt_borrow is true, print the max borrow amount retrieved
-        #         in step 1, then prompt the user to either accept the max amount
-        #         or instead enter an amount.
-
-        if self.prompt_borrow_qty:
-
-            while self.borrow_info['borrow_qty'] is None:
-                borrow_qty_input = input(  # Will change this to max amount, but it's at 1% for testing
-                    f"\nTo borrow 1% of the max amount ({round(max_borrow_size/100, 4)} {self.borrow_coin}), press enter.\n"
-                    "Otherwise, type an amount:\n")
-                if borrow_qty_input == "":
-                    self.borrow_info['borrow_qty'] = round(max_borrow_size/100, 4)
-                    self.status['msg'] = f"Borrowing 1% of max amount: {self.borrow_info['borrow_qty']} {self.borrow_coin}"
-                else:
-                    try:
-                        self.borrow_info['borrow_qty'] = float(borrow_qty_input)
-
-                        self.status['msg'] = f"Borrowing {borrow_qty_input} {self.borrow_coin}\n"
-                    except:
-                        self.status['msg'] = 'Invalid Number entered. Trying again..'
-
-        else:
-            self.status['msg'] = f"Borrowing max amount: {max_borrow_size} {self.borrow_coin}\n"
-            self.borrow_info['borrow_qty'] = max_borrow_size
-
-        print(self.status['msg'])
-        # ----------------------------------------
-        # Step 3: Borrow from the exchange in the desired quantity
-        if 'borrow' in self.exchange.has and self.exchange.has['borrow']:  # If the key is present and is True
-            self.borrow_order = self.exchange.borrow(self.borrow_coin, self.borrow_info['borrow_qty'])
-
-            self.status['msg'] = f"Borrowed {self.borrow_info['borrow_qty']} {self.borrow_coin}\n"
-            print(self.status['msg'])
-
-        else:
-            raise ValueError(f"{self.exchange_id} doesn't have a 'borrow' method")
+        self.borrow()
 
         # ----------------------------------------
         # Step 4: Sell quote pair using the coins borrowed in step 3.
@@ -188,8 +144,10 @@ class Position(Config):
 
         current_bid_price = self.exchange.fetch_ticker(self.quote_pair)
         self.buy_order = self.exchange.place_margin_order('buy', self.base_pair, available_balance, order_type)
+        self.trades_info['open'][''] = self.exchange.fetch_available_margin_balance(self.synth_pair_tuple[1])
+
         self.status['msg'] = (f"Opened {order_type} BUY order on {self.base_pair}.\n"
-                       f"Quantity: {available_balance}")
+                              f"Bought {self.synth_pair_tuple[1]} with {available_balance} {self.synth_pair_tuple[2]}")
         print(self.status['msg'])
 
         if self.debug_mode:
@@ -205,70 +163,18 @@ class Position(Config):
         #         borrow_coin will be base coin of the base_pair
         #          e.g. in ETHUSDT/BTCUSDT it is ETH
 
+        self.trades_info['open']['base_pair']['side'] = 'buy'
+        self.trades_info['open']['base_pair']['symbol'] = self.base_pair
+
+        self.trades_info['open']['quote_pair']['side'] = 'sell'
+        self.trades_info['open']['quote_pair']['symbol'] = self.quote_pair
+
         self.borrow_coin = self.get_borrow_coin(self.synth_pair_tuple, 'SHORT')
         self.borrow_info['currency'] = self.borrow_coin
         self.status['msg'] = f"Borrow coin for {self.synth_pair} is: {self.borrow_coin}"
 
-        self.status['msg'] = f"Checking which methods {self.exchange_name} has for margin...\n"
-        if self.exchange.has['fetchBorrowRate']:
-            self.status['msg'] = f"{self.exchange_name} has fetchBorrowRate"
-            # available_margin = self.exchange.fetch_borrow_rate(self.borrow_coin)
-        else:
-            self.status['msg'] = (f"{self.exchange_name} doesn't have fetchBorrowRate.\n"
-                           f"Checking if {self.exchange_name} has fetchMaxBorrowSize..\n"
-                           f"Note: This is a non-standard method. If possible, find a way "
-                           f"to use fetchBorrowRate instead, or some other method. "
-                           f"The only exchange which has the method `fetchMaxBorrowSize`"
-                           " is kucoin_extended, but if others are added, this will find it.")
+        self.borrow()
 
-            if self.exchange.has['fetchMaxBorrowSize']:
-                self.status['msg'] = f"{self.exchange_name} has fetchMaxBorrowSize."
-
-                max_borrow_size = round(float(self.exchange.fetch_max_borrow_size(self.borrow_coin)), 2)
-                self.status['msg'] = f"\nMax borrow amount: {max_borrow_size}  {self.borrow_coin}"
-                print("--------------------------------\n"
-                      f"{self.status['msg']}")
-            else:
-                raise Exception(
-                    f"{self.exchange_name} doesn't have fetchMaxBorrowSize or fetchBorrowRate. Can't proceed.")
-        # ----------------------------------------
-        # Step 2: TODO (Can copy/paste/modify from open_buy)
-        #         If prompt_borrow is true, print the max borrow amount retrieved
-        #         in step 1, then prompt the user to either accept the max amount
-        #         or instead enter an amount.
-        if self.prompt_borrow_qty:
-
-            while self.borrow_info['borrow_qty'] is None:
-                borrow_qty_input = input(  # Will change this to max amount, but it's at 1% for testing
-                    f"\nTo borrow 1% of the max amount ({round(max_borrow_size/100, 2)} {self.borrow_coin}), press enter.\n"
-                    "Otherwise, type an amount:\n")
-                if borrow_qty_input == "":
-                    self.borrow_info['borrow_qty'] = round(max_borrow_size/100, 2)
-                    self.status['msg'] = f"Borrowing 1% of max amount: {self.borrow_info['borrow_qty']} {self.borrow_coin}"
-                else:
-                    try:
-                        self.borrow_info['borrow_qty'] = float(borrow_qty_input)
-
-                        self.status['msg'] = f"Borrowing {borrow_qty_input} {self.borrow_coin}\n"
-                    except:
-                        self.status['msg'] = 'Invalid Number entered. Trying again..'
-
-        else:
-            self.status['msg'] = f"Borrowing max amount: {max_borrow_size} {self.borrow_coin}"
-            self.borrow_info['borrow_qty'] = max_borrow_size
-
-        print(self.status['msg'])
-        # ----------------------------------------
-        # Step 3: TODO (Can copy/paste/modify from open_buy)
-        #         Borrow from the exchange in the desired quantity
-        if 'borrow' in self.exchange.has and self.exchange.has['borrow']:  # If the key is present and is True
-            self.borrow_order = self.exchange.borrow(self.borrow_coin, self.borrow_info['borrow_qty'])
-
-            self.status['msg'] = f"Borrowed {self.borrow_info['borrow_qty']} {self.borrow_coin}\n"
-            print(self.status['msg'])
-
-        else:
-            raise ValueError(f"{self.exchange_id} doesn't have a 'borrow' method")
         # ----------------------------------------
         # Step 4: TODO (Can copy/paste/modify from open_buy)
         #         Sell base pair using the coins borrowed in step 3.
@@ -280,7 +186,7 @@ class Position(Config):
               f"{self.status['msg']}")
 
         self.status['msg'] = (f"Selling {available_balance} {self.borrow_coin} for {self.synth_pair_tuple[2]}\n"
-                       f"Opened {order_type} BUY order on {self.base_pair}.\n"
+                       f"Opened {order_type} SELL order on {self.base_pair}.\n"
                        f"Quantity: {available_balance}")
         print(self.status['msg'])
         current_bid_price = self.exchange.fetch_ticker(self.quote_pair)
@@ -315,6 +221,86 @@ class Position(Config):
               f'TODO: Implement logic for def close() in position_manager.py')
 
         return self, 0
+
+    def borrow(self):
+        self.status['msg'] = f"Checking which methods {self.exchange_name} has for margin...\n"
+        if self.exchange.has['fetchBorrowRate']:
+            self.status['msg'] = f"{self.exchange_name} has fetchBorrowRate"
+            # available_margin = self.exchange.fetch_borrow_rate(self.borrow_coin)
+            self.status['ok'] = True
+        else:
+            self.status['msg'] = (f"{self.exchange_name} doesn't have fetchBorrowRate.\n"
+                                  f"Checking if {self.exchange_name} has fetchMaxBorrowSize..\n"
+                                  f"Note: This is a non-standard method. If possible, find a way "
+                                  f"to use fetchBorrowRate instead, or some other method. "
+                                  f"The only exchange which has the method `fetchMaxBorrowSize`"
+                                  " is kucoin_extended, but if others are added, this will find it.")
+
+            if self.exchange.has['fetchMaxBorrowSize']:
+                self.status['msg'] = f"{self.exchange_name} has fetchMaxBorrowSize."
+
+                max_borrow_size = float(self.exchange.fetch_max_borrow_size(self.borrow_coin))
+
+                self.status['msg'] = f"\nMax borrow amount: {max_borrow_size}  {self.borrow_coin}"
+                self.status['ok'] = True
+
+                print("--------------------------------\n"
+                      f"{self.status['msg']}")
+            else:
+                self.status['ok'] = False
+                raise Exception(
+                    f"{self.exchange_name} doesn't have fetchMaxBorrowSize or fetchBorrowRate. Can't proceed.")
+        self.status['borrow_info'] = self.borrow_info
+        # ----------------------------------------
+        # Step 2: If prompt_borrow is true, print the max borrow amount retrieved
+        #         in step 1, then prompt the user to either accept the max amount
+        #         or instead enter an amount.
+
+        if self.prompt_borrow_qty:
+
+            while self.borrow_info['borrow_qty'] is None:
+                borrow_qty_input = input(  # Will change this to max amount, but it's at 1% for testing
+                    f"\nTo borrow 1% of the max amount ({round(max_borrow_size / 100, 4)} {self.borrow_coin}), press enter.\n"
+                    "Otherwise, type an amount:\n")
+                if borrow_qty_input == "":
+                    self.borrow_info['borrow_qty'] = round(max_borrow_size / 100, 4)
+                    self.status[
+                        'msg'] = f"Borrowing 1% of max amount: {self.borrow_info['borrow_qty']} {self.borrow_coin}"
+                else:
+                    try:
+                        self.borrow_info['borrow_qty'] = float(borrow_qty_input)
+
+                        self.status['msg'] = f"Borrowing {borrow_qty_input} {self.borrow_coin}\n"
+                        self.status['ok'] = True
+                    except:
+                        self.status['ok'] = False
+                        self.status['msg'] = 'Invalid Number entered. Trying again..'
+
+        else:
+            self.status['msg'] = f"Borrowing max amount: {max_borrow_size} {self.borrow_coin}\n"
+            self.borrow_info['borrow_qty'] = max_borrow_size
+
+        self.status['borrow_info'] = self.borrow_info
+        self.status['ok'] = True
+
+        print(self.status['msg'])
+        # ----------------------------------------
+        # Step 3: Borrow from the exchange in the desired quantity
+        if 'borrow' in self.exchange.has and self.exchange.has['borrow']:  # If the key is present and is True
+            self.borrow_order = self.exchange.borrow(self.borrow_coin, self.borrow_info['borrow_qty'])
+            self.borrow_info['orderId'] = self.borrow_order['data']['orderId']
+            self.borrow_info['borrow_timestamp_utc'] = datetime.utcnow()
+
+            self.status['borrow_info'] = self.borrow_info
+            self.status['msg'] = f"Borrowed {self.borrow_info['borrow_qty']} {self.borrow_coin}\n"
+            self.status['ok'] = True
+            print(self.status['msg'])
+
+        else:
+            self.status['ok'] = False
+            raise ValueError(f"{self.exchange_id} doesn't have a 'borrow' method")
+
+        self.status['borrow_info'] = self.borrow_info
 
 
 class PositionManager(Config):
@@ -356,6 +342,6 @@ class PositionManager(Config):
 
 
 
-# DEBUG
+
 
 
