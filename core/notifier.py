2a297c46f000fb0d21da7995b0e0f2d4736d8d86
Jonathon Quick <jonathon@spliff.li>
2022-03-30 18:01:33 +0200

    got telegram to send a message, didn't realize it had worked but now I've changed it so I need to reverse the changes until it works again

--- core/notifier.py
+++ core/notifier.py
@@ -2,13 +2,63 @@
 
 from core.position_manager import PositionManager
 
+import asyncio
+from pyrogram import Client
+from telethon import TelegramClient, events
+from telethon.errors import SessionPasswordNeededError
+
+
 class Notifier:
 
-    def __init__(self, pm: PositionManager):
+    def __init__(self: object, pm: PositionManager) -> None:
+        self.pm = pm
+        self.loop = asyncio.get_event_loop()
+
+        self.pyrogram_tg_client = Client(self.pm.api_keys['telegram']['username'],
+                                         self.pm.api_keys['telegram']['app_api_id'],
+                                         self.pm.api_keys['telegram']['api_hash'])
+
+        self.telethon_tg_client = TelegramClient(pm.api_keys['telegram']['username'],
+                                                 pm.api_keys['telegram']['app_api_id'],
+                                                 pm.api_keys['telegram']['api_hash'])
         pm.event_handler.link(self.send_telegram_notification, 'onOpenPosition')
         pm.event_handler.link(self.send_telegram_notification, 'onClosePosition')
+        self.schedule_tasks()
+        self.send_telegram_notification()
 
-    def send_telegram_notification(self, position):
+
+    def schedule_tasks(self) -> None:
+        """ Minimal selection of tasks for this demonstation. """
+        self.loop.create_task(self.pyrogram_send_msg())
+        self.loop.run_until_complete(self.pyrogram_send_msg())
+        self.loop.create_task(self.telethon_authenticate_telegram(self.pm.api_keys['telegram']['phone']))
+
+
+    def send_telegram_notification(self, position=None):
         print("Sending Telegram Notification...\n"
               "Position Info:\n")
         print(position)
+        breakpoint()
+        self.loop.create_task(self.telethon_authenticate_telegram(self.pm.api_keys['telegram']['phone']))
+        breakpoint()
+
+    async def telethon_authenticate_telegram(self, phone):
+        if not await self.telethon_tg_client.is_user_authorized():
+            await self.telethon_tg_client.send_code_request(phone)
+            try:
+                await self.telethon_tg_client.sign_in(phone, input('Enter the code: '))
+            except SessionPasswordNeededError:
+                await self.telethon_tg_client.sign_in(password=input('Password: '))
+
+        await self.telethon_tg_client.send_message(entity=self.api_keys['telegram']['notification_channel_id'],
+                                                   message='hello telegram')
+
+    async def telethon_run_in_background(self):
+        pass
+
+
+    async def pyrogram_send_msg(self):
+        async with self.pyrogram_tg_client as tg:
+            await tg.send_message('jonathon_test', 'hello')
+
+
